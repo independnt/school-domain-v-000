e0f75aff43d8153046f247d0fc61d71ee3d04e32
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-12-07 23:27:58 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,4 +7,16 @@ class School
     @roster = {}
   end
 
+  def add_student(student, grade)
+    if @roster !include?(grade)
+      @roster[:grade] = []
+      @roster[:grade] << student
+    else
+      @roster[:grade] << student
+    end
+  end
+
+
+
+
 end
